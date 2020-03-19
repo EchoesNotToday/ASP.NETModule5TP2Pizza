d1ec8a9d94656b40ca8d74428b432cd34dc3dea7
Eliez MAIGNÉ <emaign@gmail.com>
2020-03-19 15:52:58 +0100

    Partie_2 ADD validation

--- TP-Pizza/TP-Pizza/Controllers/PizzaController.cs
+++ TP-Pizza/TP-Pizza/Controllers/PizzaController.cs
@@ -52,15 +52,26 @@ namespace TP_Pizza.Controllers
         {
             try
             {
-                Pizza pizza = pVM.Pizza;
-                pizza.Pate = toutesLesPates.FirstOrDefault(p => p.Id == pVM.SelectedPate);
-                foreach (int ingredientId in pVM.SelectedIngrdients)
+                if (ModelState.IsValid)
                 {
-                    pizza.Ingredients.Add(tousLesIngredients.FirstOrDefault(i => i.Id == ingredientId));
+                    if (IsPizzaValid(pVM))
+                    {
+                        Pizza pizza = pVM.Pizza;
+                        pizza.Pate = toutesLesPates.FirstOrDefault(p => p.Id == pVM.SelectedPate);
+                        foreach (int ingredientId in pVM.SelectedIngrdients)
+                        {
+                            pizza.Ingredients.Add(tousLesIngredients.FirstOrDefault(i => i.Id == ingredientId));
+                        }
+                        pizza.Ingredients = pizza.Ingredients.OrderBy(x => x.Id).ToList();
+                        pizza.Id = toutesLesPizzas.Count() + 1;
+                        toutesLesPizzas.Add(pizza);
+                        return RedirectToAction(Constants.VIEW_INDEX);
+                    }
                 }
-                pizza.Id = toutesLesPizzas.Count() + 1;
-                toutesLesPizzas.Add(pizza);
-                return RedirectToAction("Index");
+                pVM.ingredients = tousLesIngredients;
+                pVM.pates = toutesLesPates;
+                return View(pVM);
+
             }
             catch
             {
@@ -85,20 +96,31 @@ namespace TP_Pizza.Controllers
         {
             try
             {
-                Pizza pizza = toutesLesPizzas.FirstOrDefault(p => p.Id == pizzaVM.Pizza.Id);
-                pizza.Nom = pizzaVM.Pizza.Nom;
-                pizza.Pate = toutesLesPates.FirstOrDefault(p => p.Id == pizzaVM.SelectedPate);
-                pizza.Ingredients.Clear();
-                foreach (int ingredientId in pizzaVM.SelectedIngrdients)
+                if (ModelState.IsValid)
                 {
-                    Ingredient ingredient = tousLesIngredients.FirstOrDefault(i => i.Id == ingredientId);
-                    if (ingredient != null)
+                    if (IsPizzaValid(pizzaVM))
                     {
-                        pizza.Ingredients.Add(ingredient);
+                        Pizza pizza = toutesLesPizzas.FirstOrDefault(p => p.Id == pizzaVM.Pizza.Id);
+                        pizza.Nom = pizzaVM.Pizza.Nom;
+                        pizza.Pate = toutesLesPates.FirstOrDefault(p => p.Id == pizzaVM.SelectedPate);
+                        pizza.Ingredients.Clear();
+                        foreach (int ingredientId in pizzaVM.SelectedIngrdients)
+                        {
+                            Ingredient ingredient = tousLesIngredients.FirstOrDefault(i => i.Id == ingredientId);
+                            if (ingredient != null)
+                            {
+                                pizza.Ingredients.Add(ingredient);
+                            }
+                        }
+                        pizza.Ingredients = pizza.Ingredients.OrderBy(x => x.Id).ToList();
+                        return RedirectToAction(Constants.VIEW_INDEX);
                     }
+
                 }
-                pizza.Ingredients = pizza.Ingredients.OrderBy(x => x.Id).ToList();
-                return RedirectToAction(Constants.VIEW_INDEX);
+                pizzaVM.ingredients = tousLesIngredients;
+                pizzaVM.pates = toutesLesPates;
+                return View(pizzaVM);
+                
             }
             catch
             {
@@ -131,6 +153,43 @@ namespace TP_Pizza.Controllers
             {
                 return View();
             }
+        }
+
+        private bool IsPizzaValid(PizzaVM pizzaVM)
+        {
+            bool isValid = true;
+
+            // Name already exists 
+            if (toutesLesPizzas.Any(p => p.Nom.ToUpper() == pizzaVM.Pizza.Nom.ToUpper()))
+            {
+                isValid = false;
+                ModelState.AddModelError("", Constants.ERROR_ALREADY_EXISTS_NAME);
+            }
+            
+            // Too much or not enough ingredients
+            if (pizzaVM.SelectedIngrdients.Count() < 2 || pizzaVM.SelectedIngrdients.Count() > 5 )
+            {
+                isValid = false;
+                ModelState.AddModelError("", Constants.ERROR_INGREDIENTS_COUNT);
+            }
+
+            // Same ingredients 
+            foreach (Pizza pizza in toutesLesPizzas)
+            {
+                if (pizza.Ingredients.Select(i => i.Id).SequenceEqual(pizzaVM.SelectedIngrdients))
+                {
+                    isValid = false;
+                    ModelState.AddModelError("", Constants.ERROR_ALREADY_EXISTS_INGREDIENTS);
+                }
+            }
+
+            // No pate or pate does not exists
+            if (!toutesLesPates.Any(p => p.Id == pizzaVM.SelectedPate))
+            {
+                isValid = false;
+                ModelState.AddModelError("", Constants.ERROR_PATE_UNKNOWN);
+            }
+            return isValid;
         }
     }
 }
--- TP-Pizza/TP-Pizza/Utils/Constants.cs
+++ TP-Pizza/TP-Pizza/Utils/Constants.cs
@@ -7,12 +7,18 @@ namespace TP_Pizza.Utils
 {
     public class Constants
     {
-        
+        // View names
         public const string _PIZZA_DETAILS = "_PizzaDetails";
         public const string VIEW_INDEX = "Index";
         public const string VIEW_EDIT = "Edit";
         public const string VIEW_DETAILS = "Details";
         public const string VIEW_DELETE = "Delete";
         public const string VIEW_CREATE = "Create";
+
+        //Error messages
+        public const string ERROR_ALREADY_EXISTS_NAME = "Une pizza portant ce nom existe déjà.";
+        public const string ERROR_INGREDIENTS_COUNT = "Une pizza doit avoir au minimum 2 ingrédients et au maximum 5 ingrédients.";
+        public const string ERROR_ALREADY_EXISTS_INGREDIENTS = "Une pizza avec ces ingrédients existe déjà.";
+        public const string ERROR_PATE_UNKNOWN = "La pate sélectionnée n'existe pas.";
     }
 }
